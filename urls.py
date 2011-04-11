481b1ecf400a69779d189565f8794d02ee485499
falquaddoomi <falquaddoomi@ucla.edu>
2011-04-11 04:14:02 -0700

    Initial commit of tropo, including a rename from the typo "topoconnect" to "tropoconnect".

--- urls.py
+++ urls.py
@@ -30,6 +30,7 @@ urlpatterns = patterns('',
     # user-level apps!
     (r'^linearize/', include('cens_dev.linearize.urls')),
     (r'^taskmanager/', include('cens_dev.taskmanager.urls')),
+    (r'^tropoconnect/', include('cens_dev.tropoconnect.urls')),
     (r'^twilioconnect/', include('cens_dev.twilioconnect.urls')),
 )
 
