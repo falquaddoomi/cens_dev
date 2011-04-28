48e78cc49ae70662d67f09a9f4d0be93d917809f
falquaddoomi <falquaddoomi@ucla.edu>
2011-04-28 12:25:25 -0700

    Removed tropoconnect application entirely, moved "scripts" into the "tasks" subdirectory.

--- tropoconnect/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import patterns, include, url
-import views
-
-urlpatterns = patterns('twilioconnect.views',
-    # main and only page
-    url(r'^incoming/?', 'incoming'),
-)
