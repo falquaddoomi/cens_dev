a62ec7f6328172f28307a6158d56da5ec018423b
falquaddoomi <falquaddoomi@ucla.edu>
2011-05-05 00:30:22 -0700

    Replaced Session and ScheduledTask with TaskInstance, encapsulating the functionality of both into a single entity.

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -38,7 +38,7 @@ class Patient(models.Model):
         ('sms', 'via SMS'),
         ('email', 'via E-mail'),
         ('irc', 'via IRC'),
-    )    
+    )
     address = models.CharField(max_length=200) # phone number, but address for compatibility
     email = models.CharField(max_length=200)
     handle = models.CharField(max_length=100)
@@ -142,6 +142,7 @@ class TaskTemplate(models.Model):
     name = models.CharField(max_length=100)
     task = models.ForeignKey(Task)
     arguments = models.TextField(blank=True)
+    schedulable = models.BooleanField(blank=True,default=True)
 
     def __unicode__(self):
         return "%s" % (self.name)
@@ -154,17 +155,22 @@ class ProcessManager(models.Manager):
     def get_pending_processes(self):
         # a pending process has only incomplete scheduled tasks and no completed sessions
         qset = super(ProcessManager, self).get_query_set()
-        return qset.exclude(scheduledtask__completed=True).exclude(session__completed=True)
+        return qset.filter(taskinstance__state="pending").exclude(taskinstance__state="completed")
     
     def get_current_processes(self):
         # a current process has at least one incomplete session or scheduled task
         qset = super(ProcessManager, self).get_query_set()
-        return qset.filter(Q(session__completed=False)|Q(scheduledtask__completed=False,session__completed=True)).distinct()
-
+        return qset.filter(scheduledtask__state="running")
+    
     def get_completed_processes(self):
         # a completed process has only complete scheduled tasks and sessions
         qset = super(ProcessManager, self).get_query_set()
         return qset.exclude(scheduledtask__completed=False).exclude(session__completed=False)
+
+    def reap_empty_processes(self):
+        # removes any processes that have no referring instances
+        qset = super(ProcessManager, self).get_query_set()
+        qset.annotate(num_instances=Count('taskinstance')).filter(num_instances=0).delete()
     
 class Process(models.Model):
     name = models.CharField(max_length=100)
@@ -209,52 +215,178 @@ class Process(models.Model):
         return "%s (#%d)" % (self.name, self.id)
 
 # =================================================================
-# ==== Sessions and Logging/Data Collection
+# ==== TaskInstance (aggregated by Processes)
+# ==== DESCRIPTION:
+# ==== a taskinstance is a running instance of a task for a
+# ==== particular user, as parameterized by a tasktemplate.
 # =================================================================
 
-class SessionManager(models.Manager):
-    def get_current_sessions(self):
-        qset = super(SessionManager, self).get_query_set()
-        return qset.filter(completed_date__isnull=True, completed=False)
+class TaskInstanceManager(models.Manager):
+    def get_pending_tasks(self):
+        qset = super(TaskInstanceManager, self).get_query_set()
+        return qset.filter(schedule_date__gt=datetime.now(), status="pending")
+    
+    def get_due_tasks(self):
+        qset = super(TaskInstanceManager, self).get_query_set()
+        return qset.filter(schedule_date__lte=datetime.now(), status="pending")
+    
+    def get_running_tasks(self):
+        qset = super(TaskInstanceManager, self).get_query_set()
+        return qset.filter(status="running")
+    
+    def get_timedout_tasks(self):
+        qset = super(TaskInstanceManager, self).get_query_set()
+        return qset.filter(timeout_date__lte=datetime.now(), status="running")
+    
+    def get_completed_tasks(self):
+        qset = super(TaskInstanceManager, self).get_query_set()
+        return qset.filter(status="completed")
 
-    def get_timedout_sessions(self):
-        qset = super(SessionManager, self).get_query_set()
-        return qset.filter(completed_date__isnull=True, completed=False, timeout_date__isnull=False, timeout_date__lte=datetime.now())
+    def get_errored_tasks(self):
+        qset = super(TaskInstanceManager, self).get_query_set()
+        return qset.filter(status="errored")
 
-    def get_completed_sessions(self):
-        qset = super(SessionManager, self).get_query_set()
-        return qset.filter(completed_date__isnull=False, completed=True)
+    def create_task(self, patient, task, params, schedule_date, creator="n/a", name=None):
+        """
+        Creates a new task for a given patient, with a dict of params
+        (usually specified from filling out a TaskTemplate). The task
+        will run on schedule_date, at which point it will be promoted
+        from the "pending" status to the "running" status and actually
+        begin execution of the machine associated with its Task.
 
-class Session(models.Model):
+        A new Process will be created to encapsulate this TaskInstance, which
+        will bear the name of the chosen Task by default, but can be
+        overridden by the name parameter.
+        """
+
+        p = Process(
+            name=(name if name is not None else task.name),
+            creator=creator,
+            patient=patient
+            )
+        p.save()
+
+        t = TaskInstance(
+            patient=patient,
+            task=task,
+            mode=patient.contact_pref,
+            process=p,
+            params=params,
+            schedule_date=schedule_date
+            )
+        t.save()
+    
+class TaskInstance(models.Model):
+    # core information
     patient = models.ForeignKey(Patient)
     task = models.ForeignKey(Task)
-    mode = models.CharField(max_length=50, choices=Patient.CONTACT_PREF_CHOICES)
-    process = models.ForeignKey(Process,null=True)
-    add_date = models.DateTimeField(auto_now_add=True)
-    completed = models.BooleanField(blank=True,default=False)
-    completed_date = models.DateTimeField(blank=True,null=True)
-    timeout_date = models.DateTimeField(blank=True,null=True)
-    state = models.CharField(max_length=100)
+    mode = models.CharField(max_length=50, choices=Patient.CONTACT_PREF_CHOICES) # method of contact (e.g. email, sms)
+    process = models.ForeignKey(Process)
+    active = models.BooleanField(default=True)
 
-    objects = SessionManager()
+    # possible states
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('running', 'Running'),
+        ('completed', 'Completed'),
+        ('errored', 'Errored')
+    )
+
+    # current state of the task
+    state = models.CharField(max_length=100, default="pending", choices=STATUS_CHOICES)
+    details = models.CharField(max_length=800,blank=True,null=True)
+    # state information for the task
+    params = models.TextField(blank=True) # filled in from TaskTemplate, persistent
+    context = models.TextField(blank=True) # managed by task, possibly non-persistent
+    
+    # state change times
+    # past tense dates are dates that are set when they occur
+    # whereas future tense are events that are set prior to occurring
+    added_date = models.DateTimeField(auto_now_add=True) # pending until schedule_date arrives
+    schedule_date = models.DateTimeField(blank=True,null=True) # due until executed
+    ran_date = models.DateTimeField(blank=True,null=True) # running until completed
+    timeout_date = models.DateTimeField(blank=True,null=True) # no status, waiting to be poked
+    completed_date = models.DateTimeField(blank=True,null=True) # completed forever
+    errored_date = models.DateTimeField(blank=True,null=True) # errored forever(?)
+
+    objects = TaskInstanceManager()
 
     def get_messages(self):
         return self.sessionmessage_set.all()
 
-    def get_status(self):
-        if self.completed: return "past"
-        else: return "running"
-
-    def mark_complete(self):
-        self.completed = True
+    def mark_running(self):
+        self.state = "running"
+        self.ran_date = datetime.now()
+        self.save()
+        
+    def mark_errored(self, details):
+        self.state = "errored"
+        self.details = details
+        self.errored_date = datetime.now()
+        self.save()
+        
+    def mark_completed(self):
+        if self.state == "completed": return
+        self.state = "completed"
         self.completed_date = datetime.now()
         self.save()
+        
+    def is_pending(self):
+        return (self.schedule_date > datetime.now()) and (self.state == "pending")
+
+    def is_due(self):
+        return (self.schedule_date <= datetime.now()) and (self.state == "pending")
     
+    def is_running(self):
+        return (self.state == "running")
+    
+    def is_past(self):
+        return (self.state == "completed")
+
+    def is_errored(self):
+        return (self.state == "errored")
+
+    def get_status(self):
+        if self.is_pending(): return "pending"
+        elif self.is_due(): return "due"
+        elif self.is_running(): return "running"
+        elif self.is_past(): return "past"
+        else: return "unknown"
+
+    def spawn_task(self, task, update_params, schedule_date):
+        """
+        Spawns a new task for the current patient. The task
+        will run on schedule_date, at which point it will be promoted
+        from the "pending" status to the "running" status and actually
+        begin execution of the machine associated with its Task.
+
+        The new task will have the same process as the existing task,
+        and its params will be the existing parameters updated by the
+        specified ones.
+        """
+
+        # copy existing parameters and update them with the new params
+        new_params = dict(self.params)
+        new_params.update(update_params)
+
+        t = TaskInstance(
+            patient=self.patient,
+            task=self.task,
+            mode=self.mode,
+            process=self.process,
+            params=new_params,
+            schedule_date=schedule_date
+            )
+        t.save()
+
+    class Meta:
+        verbose_name_plural = "task instances"
+        
     def __unicode__(self):
-        return "Session for %s on %s" % (self.patient.address, self.task.name)
+        return "Task Instance for %s on %s" % (self.patient.address, self.task.name)
 
 class SessionMessage(models.Model):
-    session = models.ForeignKey(Session)
+    session = models.ForeignKey(TaskInstance)
     message = models.TextField()
     outgoing = models.BooleanField()
     add_date = models.DateTimeField(auto_now_add=True)
@@ -264,53 +396,3 @@ class SessionMessage(models.Model):
             return "Sent to %s: %s" % (self.session.patient.address, self.message)
         else:
             return "Received from %s: %s" % (self.session.patient.address, self.message)
-
-# =================================================================
-# ==== Scheduled Tasks
-# =================================================================
-
-class ScheduledTaskManager(models.Manager):
-    def get_pending_tasks(self):
-        qset = super(ScheduledTaskManager, self).get_query_set()
-        return qset.filter(schedule_date__gt=datetime.now(), active=True, completed=False)
-
-    def get_due_tasks(self):
-        qset = super(ScheduledTaskManager, self).get_query_set()
-        return qset.filter(schedule_date__lte=datetime.now(), active=True, completed=False)
-
-    def get_past_tasks(self):
-        qset = super(ScheduledTaskManager, self).get_query_set()
-        return qset.filter(schedule_date__lte=datetime.now(), active=True, completed=True)
-
-class ScheduledTask(models.Model):
-    patient = models.ForeignKey(Patient)
-    task = models.ForeignKey(Task)
-    process = models.ForeignKey(Process,null=True)
-    add_date = models.DateTimeField(auto_now_add=True)
-    arguments = models.TextField(blank=True)
-    schedule_date = models.DateTimeField()
-    active = models.BooleanField(blank=True, default=True)
-    completed = models.BooleanField(blank=True,default=False)
-    completed_date = models.DateTimeField(blank=True,null=True)
-    result = models.TextField(blank=True)
-
-    objects = ScheduledTaskManager()
-
-    def is_pending(self):
-        return (self.schedule_date > datetime.now()) and (not self.completed)
-
-    def is_due(self):
-        return (self.schedule_date <= datetime.now()) and (not self.completed)
-
-    def is_past(self):
-        return (self.schedule_date <= datetime.now()) and (self.completed)
-
-    def get_status(self):
-        if self.is_pending(): return "pending"
-        elif self.is_due(): return "due"
-        elif self.is_past(): return "past"
-        else: return "unknown"
-
-    def __unicode__(self):
-        return "Scheduled Task for %s on %s" % (self.patient.address, self.task.name)
-
