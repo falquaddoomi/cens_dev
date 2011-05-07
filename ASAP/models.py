6f92e44d33daede80e6620a3935f08f9d02fef3b
falquaddoomi <falquaddoomi@ucla.edu>
2011-05-07 13:20:44 -0700

    Moved static files from taskmanager to ASAP.

--- ASAP/models.py
+++ ASAP/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class ASAPParticipant(models.Model):
+    name = models.CharField(max_length=100)
+    birth_date = models.DateField(blank=True, null=True)
+
+    def __unicode__(self):
+        return self.name
