5096f32aeb44ad545f2839693be9d58cfa5c9b7b
falquaddoomi <falquaddoomi@ucla.edu>
2011-05-08 01:54:10 -0700

    Added ASAP templates, views to the ASAP site.

--- ASAP/views.py
+++ ASAP/views.py
@@ -18,25 +18,42 @@ import parsedatetime.parsedatetime_consts as pdc
 # for parsing argument lists
 import json, urllib
 
-from django.db import models
-from django.forms import ModelForm
-
-class ASAPForm(ModelForm):
-    class Meta:
-        model = ASAPParticipant
-
 @csrf_protect
 def signupform(request):
     if request.method == 'POST': # If the form has been submitted...
-        form = ASAPForm(request.POST) # A form bound to the POST data
-        if form.is_valid(): # All validation rules pass
-            name = form.cleaned_data['name']
-            birth_date = form.cleaned_data['birth_date']
+        # form = ASAPParticipantForm(request.POST) # A form bound to the POST data
+        # if form.is_valid(): # All validation rules pass
+        #    subject = form.cleaned_data['subject']
+
+        # create a Patient for them, too
+        np = Patient(
+            address = request.POST['cellphone'],
+            first_name = request.POST['firstname'],
+            last_name = request.POST['lastname']
+            )
+        np.save()
+        
+        # we're assigning all ASAP signups to the ASAP Admin account
+        np.clinicians.add(Clinician.objects.get(user=User.objects.get(username='asap_admin')))
             
-            return HttpResponseRedirect('/ASAP/thanks/') # Redirect after POST
+        return HttpResponseRedirect('/ASAP/thanks/') # Redirect after POST
     else:
-        form = ContactForm() # An unbound form
+        form = ASAPParticipantForm() # An unbound form
 
     return render_to_response('ASAP/signup.html', {
-            'form': form,
-        }, context_instance=RequestContext(request))
+        'form': form,
+        'goal_categories': ASAPGoalCategory.objects.all()
+    }, context_instance=RequestContext(request))
+
+def thanks(request):
+    return render_to_response('ASAP/thanks.html', {}, context_instance=RequestContext(request))
+
+# =============================================
+# ASAP has a few models it needs to run, detailed here
+# =============================================
+
+from django.forms import ModelForm
+
+class ASAPParticipantForm(ModelForm):
+    class Meta:
+        model = ASAPParticipant
