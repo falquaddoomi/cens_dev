0145ca94f57ab50692b752e0dbb30e4110dcd0b6
falquaddoomi <falquaddoomi@ucla.edu>
2011-05-08 23:00:06 -0700

    Apparently, WinSCP munged copying the ASAP files from the other project; recopied and everything seems to be in order.

--- ASAP/static/js/signup.js
+++ ASAP/static/js/signup.js
@@ -16,13 +16,11 @@ $(document).ready(function() {
 	// ====================================================================
 	// === navigation handlers
 	// ====================================================================
-
-	// associate a handler with each next link
-	// we're going to fade in the next step, then pan down to it
-	$(".nextbox a").click(function() {
+	
+	function moveToNextSection($me) {
 		// show the href'd element
 		// but first hide its inner content so we can slide it out
-		var $target = $($(this).attr("href"));
+		var $target = $($me.attr("href"));
 
 		if ($target.is(":hidden")) {
 			// only do the animation if it wasn't visible in the first place
@@ -36,10 +34,43 @@ $(document).ready(function() {
 			// just pan to the element
 			$.scrollTo($target, { duration: 600, axis: 'y' });
 		}
+	}
 
+	// associate a handler with each next link
+	// we're going to fade in the next step, then pan down to it
+	$(".nextbox a").not("a#link_to_step2, a#link_to_complete").click(function() {
+		moveToNextSection($(this));
 		return false;
 	});
-
+	
+	// step 1 is special b/c it does validation before it continues
+	$(".nextbox a#link_to_step2").click(function() {
+		if ($('#signup_form').valid()) {
+			moveToNextSection($(this));
+		}
+		else
+		{
+			$(".floater").fadeTo(0, 0, function() {
+				$(this).fadeTo(400, 1);
+			});
+		}
+		
+		return false;
+	});
+	
+	// step 3 is special as well
+	$(".nextbox a#link_to_complete").click(function() {
+		if (updateGoalMessageStatus()) {
+			moveToNextSection($(this));
+		}
+		
+		return false;
+	});
+	
+	// and step 2 should at least do the validation...
+	$("a#link_to_step3").click(function() {
+		updateGoalMessageStatus();
+	});
 
 	// ====================================================================
 	// === diagnosis checkbox list handlers
@@ -69,6 +100,46 @@ $(document).ready(function() {
 	// === goal selectors and reorderable list
 	// ====================================================================
 
+	function updateGoalMessageStatus()
+	{
+		// update the status of the "no goals" message after every selection
+		if ($(".goal_container input:radio").filter(":checked").not("[value='']").length > 0)
+		{
+			console.log("items: ",$(".goal_container input:radio").filter(":checked").not("[value='']").length);
+			
+			$("#no_goals_message").fadeOut(300);
+			return true;
+		}
+		else
+		{
+			if ($("#no_goals_message").is(":visible")) {
+				// flash the message
+				$("#no_goals_message").fadeTo(0, 0, function() {
+					$(this).fadeTo(400, 1);
+				});
+			}
+			else
+			{
+				// show it for the first time
+				$("#no_goals_message").fadeIn(300);
+			}
+			
+			return false;
+		}
+	}
+	
+	function updateGoalsList()
+	{
+		// updates the hidden field that POSTs the goals to the server at the end
+		$("#goals_list_hidden").val(
+			$("#final_goals_list").sortable("serialize", {'attribute': 'value', 'key': 'value'})
+		);
+	}
+	
+	$("#no_goals_message").click(function() {
+		$.scrollTo("#step2", { duration: 600, axis: 'y', offset: { left: 0, top: -10 } });
+	});
+
 	// bind up the controls that populate the reorderable list...
 	$(".goal_container input:radio").change(function() {
 		// find the label text that corresponds to this item
@@ -97,17 +168,42 @@ $(document).ready(function() {
 		else
 		{
 			// we have no item to remove, so simply append this new item
-			$("<li class='" + $(this).attr("name") + "'>" + $label.text() + "</li>")
+			$("<li class='" + $(this).attr("name") + "' value='" + $(this).attr("value") + "'>" + $label.text() + "</li>")
 				.hide()
 				.appendTo("#final_goals_list")
 				.fadeIn();
 		}
+		
+		updateGoalMessageStatus();
+		updateGoalsList();
 	});
 
 	// finally, bind up that reorderable goals list!
 	$("#final_goals_list").sortable({
 		axis: 'y',
-		opacity: 0.6
+		opacity: 0.6,
+		update: function(event, ui) {
+			// ensure that the hidden element has the correct value
+			updateGoalsList();
+			
+			alert($("#goals_list_hidden").val());
+		}
 	});
 	$("#final_goals_list").disableSelection();
+	
+	// =================================
+	// === deal with potential initial values on refresh
+	// =================================
+	
+	// issue an initial change event to init the goal selector to previously populated values
+	$(".goal_container input:radio").filter(":checked").change();
+	
+	// miscellanea
+	
+	$("#finished_button").hover(function() {
+		$("#finish_callout").stop().animate({ right: "-=5px" }, 600);
+	},
+	function() {
+		$("#finish_callout").stop().animate({ right: "+=5px" }, 600);
+	});
 });
